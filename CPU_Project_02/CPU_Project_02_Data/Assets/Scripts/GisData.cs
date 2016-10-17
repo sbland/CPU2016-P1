b9a613dea8b4c260fcdad64f14159444c21bf1b2
sbland <sbland.Co.UK@gmail.com>
2016-10-17 21:19:06 +0100

    Implemented classes for Wards and Tram stops
    I've set it up so that each node adds itself to a list (I.e tram_Stops)
    this can then be access  and iterated through using a foreach  to change
    an aspect of all the nodes. At the moment if you press space it switches
    the census data to "travelByBus" and if you press N it changes to
    travelByCar (All can me mapped to buttons etc.
    Also added a loading text at the bottom so you know the data is loading.

--- CPU_Project_02/CPU_Project_02_Data/Assets/Scripts/GisData.cs
+++ CPU_Project_02/CPU_Project_02_Data/Assets/Scripts/GisData.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using System.IO;
 using System.Xml;
@@ -8,100 +9,141 @@ using System.Text;
 
 public class GisDataStatics
 {
-    public static float scale;// = 400;
-    public static float offsetX;// = 397000;
-    public static float offsetY;// = 395000;
+    public static float scale;
+    public static float offsetX;
+    public static float offsetY;
+}
+
+public class GisDataObjects
+{
+    public static List<TramStop> tramStops = new List<TramStop>();
+    public static List<Ward> wards = new List<Ward>();
 }
 
 public class GisData : MonoBehaviour
 {
-    public float scale = 50;
+    
+    public float scale = 500;
     public float offsetX = 397000;
     public float offsetY = 395000;
 
-    public TextAsset gisDataFile;
+    public TextAsset[] gisDataFiles;
     public TextMesh textTitle;
-
+    public Text loadingText;
+    
 
 
     // Use this for initialization
     void Start()
     {
+        //Assigns the values set in the unity editor to the standard c# class above. These variables can then be accessed from other classes.
         GisDataStatics.scale = scale;
         GisDataStatics.offsetX = offsetX;
         GisDataStatics.offsetY = offsetY;
-        
-        LoadGisData(gisDataFile);
+
+      
+
+        LoadGisData(gisDataFiles); //On start load in the data from the xml files
 
     }
 
-    // Update is called once per frame
-    //void Update()
-    //{
-
-    //}
-
-   
-
-
-    public void LoadGisData(TextAsset xmlFileIn)
+    //Update is called once per frame
+    void Update()
     {
-        XmlDocument gisXml = new XmlDocument();
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+          
 
-        try { gisXml.LoadXml(xmlFileIn.text); }
-        catch { Debug.Log("Failed to load xml file"); }
+            foreach (Ward ward in GisDataObjects.wards)
+            {
+                ward.UpdateFromData("travelByBus");
+            }
+        }
 
-        StartCoroutine(LoadingCoroutine(gisXml)); //allows loading of assets while continuing other tasks
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            
 
-
+            foreach (Ward ward in GisDataObjects.wards)
+            {
+                ward.UpdateFromData("travelByCar");
+            }
+        }
     }
 
-    private IEnumerator LoadingCoroutine(XmlDocument inputXml)
+
+
+
+    public void LoadGisData(TextAsset[] xmlFilesIn)
+    {
+        foreach(TextAsset xmlFileIn  in xmlFilesIn)
+        {
+            XmlDocument gisXml = new XmlDocument(); //new xml var to store xml doc 
+
+            try { gisXml.LoadXml(xmlFileIn.text); }
+            catch { Debug.Log("Failed to load xml file"); } //load the xml doc into the xml var
+
+            Debug.Log(gisXml.DocumentElement.GetAttribute("name"));
+
+           StartCoroutine(LoadingCoroutine(gisXml, gisXml.DocumentElement.GetAttribute("name"))); //Start loading data from xml doc to a c# object. Also allows loading of assets while continuing other tasks
+
+        }
+
+
+        
+        
+    }
+
+    private IEnumerator LoadingCoroutine(XmlDocument inputXml, string dataTitle)
     {
         XmlNodeList nodeList = inputXml.DocumentElement.ChildNodes; // opens the first level of objects in the xml file (E.g the wards)
 
         foreach (XmlNode xmlNodeI in nodeList)
         {
+            loadingText.text = "Loading...";
             //## Load 1 item per frame
             yield return new WaitForEndOfFrame();
 
             //Load data
             float posLat = float.Parse(xmlNodeI["X"].InnerText);
             float posLong = float.Parse(xmlNodeI["Y"].InnerText);
-            float inverseScale = 500 / scale; // This sets the size of the cylinder based on the scale of the model
-            string wardName = xmlNodeI.Attributes["name"].Value;
-
-            Dictionary<string, float> censusDataDict = new Dictionary<string, float>();
+            string rowName = xmlNodeI.Attributes["name"].Value;
+            Dictionary<string, float> censusDataDict = new Dictionary<string, float>(); //create dictionary storing all data in each xml object
 
 
-
+            //Iterate over each row in the xml (Each row is a seperate ward or bus stop etc) and add the data to the dictionary
             foreach (XmlNode xmlChild in xmlNodeI)
             {
-                
-
-
-               
-                Debug.Log(xmlChild.Name);
-                float parseFloat;
-
-                
-                parseFloat = float.Parse(xmlChild.InnerText);
-                      
-                censusDataDict.Add(xmlChild.Name, parseFloat);
-
+                float parseFloat = float.Parse(xmlChild.InnerText);
+                censusDataDict.Add(xmlChild.Name, parseFloat); //I.e. ("travelByTram", "30.3")
+            }
+            
+            
+            switch(dataTitle)
+            {
+                case "Wards":
+                    //create a new object from the ward class to store each ward.
+                    Ward newWard = new Ward(rowName, posLat, posLong, censusDataDict);
+                    GisDataObjects.wards.Add(newWard);
+                    newWard.Initiate();
+                    break;
+                case "Tram_Stops":
+                    TramStop newTramStop = new TramStop(rowName, posLat, posLong, censusDataDict);
+                    GisDataObjects.tramStops.Add(newTramStop);
+                    newTramStop.Initiate();
+                    break;
+                default:
+                    break;
             }
 
 
+            
 
+        }//end foreach
 
-
-            //create a new ward
-            Ward newWard = new Ward(wardName, posLat, posLong, censusDataDict, inverseScale);
-            newWard.Initiate();
-
-        }
-
-
+        loadingText.text = "Loading...Complete";
+        yield return new WaitForSeconds(2);
+        loadingText.text = "";
         yield break;
-    }
+    }//end LoadingCoroutine
 }
--- CPU_Project_02/CPU_Project_02_Data/Assets/Scripts/Ward.cs
+++ CPU_Project_02/CPU_Project_02_Data/Assets/Scripts/Ward.cs
@@ -10,17 +10,17 @@ public class Ward
     public string wardName;
     public float latitude;
     public float longitude;
-    public float cylinderSize;
     public Dictionary<string, float> censusDataDictLocal;
-
+    float cylinderSize = 600 / GisDataStatics.scale; // This sets the size of the cylinder based on the scale of the model
+    GameObject cylinder;
 
     //Ward Constructor
-    public Ward (string wardNameIn, float latIn, float longIn, Dictionary<string, float> dataArrayIn, float cylinderSizeIn)
+    public Ward (string wardNameIn, float latIn, float longIn, Dictionary<string, float> dataArrayIn)
     {
         wardName = wardNameIn;
         latitude = latIn;
         longitude = longIn;
-        cylinderSize = cylinderSizeIn;
+        //cylinderSize = cylinderSizeIn;
         censusDataDictLocal = dataArrayIn;
     }
 
@@ -28,7 +28,7 @@ public class Ward
 
     public void Initiate ()
     {
-        GameObject cylinder = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        cylinder = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
         cylinder.name = wardName;
 
         //offset and rescale
@@ -37,12 +37,16 @@ public class Ward
 
         latitude = latitude / GisDataStatics.scale;
         longitude = longitude / GisDataStatics.scale;
-
+        
 
 
         cylinder.transform.position = new Vector3(latitude, censusDataDictLocal["travelByTram"], longitude);
         cylinder.transform.localScale = new Vector3(cylinderSize, censusDataDictLocal["travelByTram"], cylinderSize);
     }
 
-   
+    public void UpdateFromData(string inputName)
+    {
+        cylinder.transform.position = new Vector3(latitude, censusDataDictLocal[inputName], longitude);
+        cylinder.transform.localScale = new Vector3(cylinderSize, censusDataDictLocal[inputName], cylinderSize);
+    }
 }
